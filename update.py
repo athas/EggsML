56433d49d2cef9787af893d4d1b582bfa27fee36
Martin Dybdal <dybber@dybber.dk>
2010-10-13 13:32:12 +0200

    bam

--- update.py
+++ update.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from os import system
+from os import system, geteuid
+import sys
 
 class update:
-    
     def __init__(self):
         t = open('template.html')
         temp = t.read()
@@ -12,6 +12,7 @@ class update:
 
         retval = system("cd /eggsml; git pull > /dev/null")
         content = "Return value: " + str(retval)
+        sys.stderr.write("Current user: " + str(geteuid()));
         print "Content-type: text/html; charset=UTF-8\n"
         print temp.replace('{{CONTENT}}', content)
 
