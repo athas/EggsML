47acef0e08f35bca7a7e2223f4d510c377711e43
Martin Dybdal <dybber@dybber.dk>
2010-11-03 17:57:08 +0100

    Balances was not counted correctly

--- eggsml.py
+++ eggsml.py
@@ -296,7 +296,8 @@ class eggsml:
                         price = prices[d['date']]
                         for u in d['users']:
                                 info[u['user']]['balance'] -= price * u['amount']
-                                info[u['user']]['eggscount'] += 1
+                                print u
+                                info[u['user']]['eggscount'] +=  u['amount']
 
                 for useralias in info.keys():
                   dates = self.get_dates()
