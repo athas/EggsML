efd32011973270102b2b0a3a9dbddf3fdeebbe3e
Troels Henriksen <athas@sigkill.dk>
2011-03-04 14:02:01 +0100

    ønsker command.

--- eggslib/eggstool.py
+++ eggslib/eggstool.py
@@ -12,6 +12,10 @@ def print_aliases(eggs, name):
             return True
     return False
 
+def print_wishes(eggs):
+    for wish in eggs.get_wishes():
+        print wish
+
 if __name__ == '__main__':
     e = eggsml()
     if len(sys.argv) < 3:
@@ -24,5 +28,7 @@ if __name__ == '__main__':
             exit("Usage: %s %s %s <alias>" % (sys.argv[0], lunchfile, command))
         if not print_aliases(e, sys.argv[3]):
             exit(1)
+    elif command == "wishes":
+        print_wishes(e)
     else:
         exit("Unrecognized command %s" % command)
