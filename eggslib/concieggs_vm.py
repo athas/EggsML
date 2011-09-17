f86184a28170d31a58be0359da283696383114fd
Frej Soya <frej.soya@gmail.com>
2011-09-18 01:21:11 +0200

    add seconds from epoch as arg

--- eggslib/concieggs_vm.py
+++ eggslib/concieggs_vm.py
@@ -3,9 +3,10 @@
 import subprocess
 from subprocess import Popen
 import os
+from time import time
 
-#DIR = os.path.join(os.getcwd(),'concieggs')
-DIR = '/eggsml/concieggs/'
+DIR = os.path.join(os.getcwd(),'concieggs')
+#DIR = '/eggsml/concieggs'
 CMDDIR  = os.path.join(DIR,'cmds')
 DBDIR  = os.path.join(DIR,'db')
       
@@ -19,13 +20,14 @@ ENV = {'CONCIEGGS_DIR' : DIR
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-def run(cmd,user):
+def run(cmd,user,args=None):
+  if args is None: args=[] #
   os.environ.update(ENV)
   os.environ['EGGS_USER'] = user
   os.environ['EGGS_LINE'] = 'dummy'
   cmdline = os.path.join(CMDDIR,cmd)
-  
-  process = Popen(cmdline,stdout=subprocess.PIPE)
+  args = [str(int(time()))]
+  process = Popen([cmdline]+args,stdout=subprocess.PIPE)
   output, unused_err = process.communicate() 
   retcode = process.poll()
 
