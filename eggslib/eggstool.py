339d3b152a55a0ceeb76a92ba2c5450e43f04a57
Troels Henriksen <athas@sigkill.dk>
2011-11-09 11:09:49 +0100

    Add lunches command to eggstool.

--- eggslib/eggstool.py
+++ eggslib/eggstool.py
@@ -4,12 +4,17 @@
 from eggsml import eggsml
 import sys
 
-def print_aliases(eggs, name):
+def user_aliases(eggs, name):
     for user_aliases in eggs.aliases:
         if name.lower() in map(lambda (x): x.lower(), user_aliases):
-            for alias in user_aliases:
-                print alias
-            return True
+            return user_aliases
+
+def print_aliases(eggs, name):
+    aliases = user_aliases(eggs,name)
+    if aliases != None:
+        for alias in aliases:
+            print alias
+        return True
     return False
 
 def print_wishes(eggs):
@@ -20,6 +25,16 @@ def print_balances(eggs):
     userinfo = eggs.get_userinfo()
     for v in userinfo:
         print round(userinfo[v]['balance'], 2), eggs.get_alias_rand(v)
+
+def print_lunches(eggs, name):
+    dates = eggs.get_dates()
+    aliases = user_aliases(eggs,name)
+    if aliases == None:
+        return False
+    for d in reversed(dates):
+        for u in d['users']:
+            if u['user'] in aliases:
+                print d['date']
 
 if __name__ == '__main__':
     e = eggsml()
@@ -37,5 +52,10 @@ if __name__ == '__main__':
         print_wishes(e)
     elif command == "balances":
         print_balances(e)
+    elif command == "lunches":
+        if len(sys.argv) != 4:
+            exit("Usage: %s %s %s <alias>" % (sys.argv[0], lunchfile, command))
+        if not print_lunches(e, sys.argv[3]):
+            exit(1)
     else:
         exit("Unrecognized command %s" % command)
