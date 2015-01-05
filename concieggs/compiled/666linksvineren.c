6035544dffdda3c97e67918657d801fe6514c688
Troels Henriksen <athas@sigkill.dk>
2015-01-05 21:01:15 +0100

    Slightly less crap.

--- concieggs/compiled/666linksvineren.c
+++ concieggs/compiled/666linksvineren.c
@@ -3,23 +3,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <sys/time.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <time.h>
 
 int linksfd;
 char* links;
 int linkslength;
-struct timeval now;
+time_t now;
 char* eggsuser;
 int eggsuserlength;
+
+char buf[1024];
 
 void checklink(const char* link) {
   int l = strlen(link);
   char* foundlink = (char*)memmem(links, linkslength, link, l);
+  int linestrlen;
   if (foundlink != NULL) {
     char* end = foundlink;
     while (!isspace(*end) && *end != 0) {
@@ -28,9 +33,6 @@ void checklink(const char* link) {
     end = 0;
     if (*(foundlink + l) == ' ') {
       char* nstart = foundlink + l + 1;
-      struct timeval then;
-      then.tv_sec = atol(nstart);
-      then.tv_usec = 0;
       char* whostart = strstr(nstart + 1, " ") + 1;
       char* whoend = strstr(whostart, "\n");
       *whoend = 0;
@@ -45,53 +47,67 @@ void checklink(const char* link) {
       /*printf("OOOOOOOOLDZ %s af %s for %ld dage siden\n", link, whostart, (now.tv_sec - then.tv_sec)/60/60/24);*/
     }
   }
-  // Always log
-  write(linksfd, link, l);
-  write(linksfd, " ", 1);
-  char nowtext[24];
-  sprintf(nowtext, "%ld", now.tv_sec);
-  write(linksfd, nowtext, strlen(nowtext));
-  write(linksfd, " ", 1);
-  write(linksfd, eggsuser, eggsuserlength);
-  write(linksfd, "\n", 1);
+  /* Always log */
+  linestrlen = snprintf(buf, sizeof(buf), "%s %ld %s\n", link, now, eggsuser);
+  write(linksfd, buf, linestrlen-1);
 }
 
 int main() {
+  char* eggsbody;
+
   if (system("shuttingUp") == 0) {
     return 0;
   }
-  char* eggsbody = getenv("EGGS_BODY");
+
+  eggsbody = getenv("EGGS_BODY");
   eggsuser = getenv("EGGS_USER");
-  srand(time(NULL));
+
+  now = time(NULL);
+
+  srand(now);
+
   if (eggsbody != NULL && eggsuser != NULL) {
-    gettimeofday(&now, NULL);
-    eggsuserlength = strlen(eggsuser);
-    char linkspath[1024];
-    sprintf(linkspath, "%s/links", getenv("CONCIEGGS_DB_DIR"));
-    linksfd = open(linkspath, O_RDWR | O_APPEND | O_CREAT);
     struct stat fs;
+
+    eggsuserlength = strlen(eggsuser);
+    sprintf(buf, "%s/links", getenv("CONCIEGGS_DB_DIR"));
+    linksfd = open(buf, O_RDWR | O_APPEND | O_CREAT);
     fstat(linksfd, &fs);
     linkslength = fs.st_size;
     links = mmap(NULL, linkslength, PROT_WRITE | PROT_READ, MAP_PRIVATE, linksfd, 0);
     while (1) {
-      char* start = strstr(eggsbody, "http://");
+      char* start, *end;
+      int atend;
+
+      start = strstr(eggsbody, "http://");
+
       if (start == NULL) {
         start = strstr(eggsbody, "https://");
       }
+
       if (start == NULL) {
         return 0;
       }
-      char* end = start;
+
+      end = start;
+
       while (*end != ' ' && *end != 0) {
         end++;
       }
-      int atend = *end == 0;
+
+      atend = *end == 0;
       *end = 0;
+
       checklink(start);
+
       if (atend) {
         return 0;
       }
+
       eggsbody = end+1;
     }
+    return 0;
+  } else {
+    return 1;
   }
 }
