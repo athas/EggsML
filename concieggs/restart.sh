0bf3c3efecbb887d7d982711f863f3f62892507b
Niels G. W. Serup <ngws@metanohi.name>
2015-03-03 14:43:06 +0100

    restart script

---
+++ concieggs/restart.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Restart one of concieggsd's subsystems.
+
+if [ $# -lt 1 ]; then
+    echo usage: $0 event|irc
+fi
+
+case "$1" in
+    event)
+        kill $(ps | grep irc_event_reader.awk | cut -d ' ' -f 1)
+        ;;
+    irc)
+        pkill sic
+        ;;
+    *)
+        exit 1
+        ;;
+esac
