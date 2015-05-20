8d1e8eee8bcf95804a63a896182de5ee69ba3c26
Thorkil Værge <thorkilk@gmail.com>
2015-05-21 00:24:59 +0200

    Update vejr.go

--- concieggs/compiled/vejr.go
+++ concieggs/compiled/vejr.go
@@ -24,6 +24,7 @@ const (
 	DIKULAT    = 55.702082 //breddegrad for DIKUs Kantine
 	MEAN_EARTH_RADIUS = 6371 //enhed er i kilometer!
 	kantinevejrBeskrivelse = "Kantinen er inden døre, og der er derfor nok tørvejr og stuetemperatur. Men det ved du nok bedre end jeg!"
+	fejlVedVejrBeskrivelse = "Jeg kender ikke denne vejrbeskrivelse!"
 )
 
 
@@ -71,6 +72,94 @@ type JsonAPI struct {
 func projicerVindretning(koordinat int, hovedretning, oploesning int) bool {
 	return math.Abs(float64(koordinat)-float64(hovedretning)) <= float64(oploesning)
 }
+
+/* Fortolker vejrkode og returnerer en vejrbeskrivelse */
+func vejrbeskrivelse(kode int) string {
+	// Vejrbeskrivelser er hentet fra openweathermap.org/weather-conditions
+vejrBeskrivelse := map[string]int{
+    200: "tordenvejr med let regn",
+    201: "tordenvejr med regn",
+    202: "tordenvejr med kraftig regn",
+    210: "let tordenvejr",
+    211: "tordenvejr",
+    212: "kraftigt tordenvejr",
+    221: "omskifteligt tordenvejr",
+    230: "tordenvejr med let støvregn",
+    231: "tordenvejr med støvregn",
+    232: "tordenvejr med kraftig støvregn",
+    300: "let støvregn",
+    301: "støvregn",
+    302: "kraftig støvregn",
+    310: "let støvregn/regn",
+    311: "støvregn/regn",
+    312: "kraftig støvregn/regn",
+    313: "regnbyger og støvregn",
+    314: "kraftige regnbyger og støvregn",
+    321: "byger med støvregn",
+    500: "let regn",
+    501: "regn",
+    502: "kraftig regn",
+    503: "skybrud",
+    504: "kraftigt skybrud",
+    511: "isslag",
+    520: "lette regnbyger",
+    521: "regnbygevejr",
+    522: "kraftige regnbyger",
+    531: "omskifteligt med byger og regn",
+    600: "let sne",
+    601: "sne",
+    602: "kraftig sne",
+    611: "slud",
+    612: "sludbyger",
+    615: "let regn og sne",
+    616: "regn og sne",
+    620: "lette snebyger",
+    621: "snebyger",
+    622: "kraftige snebyger",
+    701: "dis",
+    711: "røg",
+    721: "dis/tåge",
+    731: "sand- og støvvind",
+    741: "tåge",
+    751: "sand i luften",
+    761: "støv i luften",
+    762: "vulkansk aske",
+    771: "kastevinde",
+    781: "tornadoer",
+    800: "klar himmel",
+    801: "næsten klar himmel",
+    802: "spredte skyer",
+    803: "næsten overskyet",
+    804: "overskyet",
+    900: "tornadoer",
+    901: "tropisk storm",
+    902: "orkan",
+    903: "ekstrem kulde",
+    904: "ekstrem varme",
+    905: "ekstreme vindforhold",
+    906: "haglvejr",
+    951: "stille eller næsten stille", //herfra og ned er Beaufort-skala. Ændr dem ikke!
+    952: "svag vind",
+    953: "let vind",
+    954: "jævn vind",
+    955: "frisk vind",
+    956: "hård vind",
+    957: "stiv kuling",
+    958: "hård kuling",
+    959: "stormende kuling",
+    960: "storm",
+    961: "stærk storm",
+    962: "orkan",
+}	
+
+beskrivelse := vejrBeskrivelse[kode]
+	if (beskrivelse == "") {
+		return fejlVedVejrBeskrivelse	
+	}	
+return beskrivelse
+}
+
+
 
 /* Returner Beaufort vindskala beskrivelse (f.eks. hård vind/stiv kuling) fra funktionsargumentet, som er vindhastighed i m/s */
 func beaufort(windSpeed float64) string {
@@ -208,7 +297,8 @@ func main() {
 	/* Tid for opdatering */
 	timeForUpdate := dat.Dt
 	age := (int(time.Now().UTC().Unix()) - int(timeForUpdate))/60 //dividering med 60 angiver minutter
-
+	
+	
 
 	windDirectionstr := windDirectionString(int(windDirection))
 
