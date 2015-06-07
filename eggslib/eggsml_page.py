349f121fe686ccda3232f1219f3fcd39f2322c91
Thor <thorkilk@gmail.com>
2015-06-07 18:17:58 +0200

    forsøg med spindel-tilmelding7

--- eggslib/eggsml_page.py
+++ eggslib/eggsml_page.py
@@ -162,8 +162,8 @@ class eggsml_page:
         o = '<h1>Brainfuck pr&aelig;senterer Brainfuck\'s EggsML</h1>'
         o += '<h2>Nu uden Brainfuck!</h2>'
         o += '<h3>Tilmeld og indvi nye personer -- Virker EGGE endnu</h3>'
-        #o += '<b>Tilmeld indviet person til næste eggs:</b><br><form action="/" method="get"><input type="text" name="tilmeld"><br>'
-        #o += '<input type="submit" value="Submit"></form>'
+        o += '<b>Tilmeld indviet person til næste eggs:</b><br><form action="/" method="get"><input type="text" name="tilmeld"><br>'
+        o += '<input type="submit" value="Submit"></form>'
         o += self.eggsml_tilmeld()
         o += self.neggst()
         o += '<a href="./graph_timeline.aspeggs">Se graf over deltagelse til Eggs</a>.'
