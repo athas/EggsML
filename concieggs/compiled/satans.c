a3847019cc6ea974f278a47a997a8727b79fdc09
Troels Henriksen <athas@sigkill.dk>
2015-06-12 10:51:44 +0200

    Satans!

---
+++ concieggs/compiled/satans.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <ctype.h>
+
+static unsigned int satans_chance = 5; /* In percent. */
+
+int main() {
+  int c;
+  int inword = 0;
+  unsigned int seed;
+
+  seed ^= time(NULL);
+  seed ^= getpid();
+  seed ^= getppid();
+  srand(seed);
+
+  while ((c = getchar()) != EOF) {
+    if (inword && !isalpha(c) && rand() % 100 < satans_chance) {
+      puts("...");
+      sleep(2);
+      puts("Satans!");
+      return 0;
+    }
+    if (isalpha(c)) {
+      inword = 1;
+    } else {
+      inword = 0;
+    }
+    putchar(c);
+  }
+}
