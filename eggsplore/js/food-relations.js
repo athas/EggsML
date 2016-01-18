73de651ef8b06ebfa1843457c16466423497b8f5
Niels G. W. Serup <ngws@metanohi.name>
2016-01-18 16:37:27 +0100

    uvigtig ensretning

--- eggsplore/js/food-relations.js
+++ eggsplore/js/food-relations.js
@@ -14,18 +14,18 @@ var HEIGHT = window.innerHeight;
 
 
 function probability_to_color(probability){
-    if (probability > 0.6){return "green";}
-    if (probability > 0.4){return "yellow";}
-    if (probability > 0.2){return "orange";}
-    else {return "red"}
-    }
+    if (probability > 0.6){return 'green';}
+    if (probability > 0.4){return 'yellow';}
+    if (probability > 0.2){return 'orange';}
+    else {return 'red'}
+}
 
 function probability_to_strokewidth(probability){
-    if (probability > 0.6){return "40px";}
-    if (probability > 0.4){return "30px";}
-    if (probability > 0.2){return "20px";}
-    else {return "10px"}
-    }
+    if (probability > 0.6){return '40px';}
+    if (probability > 0.4){return '30px';}
+    if (probability > 0.2){return '20px';}
+    else {return '10px'}
+}
 
 function setup_page() {
     json_to_object(bonds_url, function(bonds) {
@@ -48,12 +48,12 @@ function find_max_value(list){
         }
     });
     return highest;
-    }
+}
 
 function build_page(bonds, densities) {
     var graph = new Object();
-    graph["nodes"] = new Array();
-    graph["links"] = new Array();
+    graph['nodes'] = new Array();
+    graph['links'] = new Array();
 
     wrapper = d3.select('#wrapper');
 
@@ -66,11 +66,11 @@ function build_page(bonds, densities) {
         ware_name = d[0];
         density = d[1];
         var ware = new Object();
-        nodeById.set(i, ware_name); 
-        ware["id"] = nodeById.set(ware_name, i);
-        ware["ware_name"] = ware_name;
-        ware["density"] = density;
-        graph["nodes"].push(ware);
+        nodeById.set(i, ware_name);
+        ware['id'] = nodeById.set(ware_name, i);
+        ware['ware_name'] = ware_name;
+        ware['density'] = density;
+        graph['nodes'].push(ware);
     });
 
     $.each(bonds, function(i, d){
@@ -80,12 +80,12 @@ function build_page(bonds, densities) {
         probability = d[2];
 
         var link = new Object();
-        link["source"] = nodeById.get(ware);
-        link["target"] = nodeById.get(other_ware);
-        link["probability"] = probability;
-        link["stroke"] = probability_to_color(probability);
-        link["strokewidth"] = probability_to_strokewidth(probability);
-        graph["links"].push(link);
+        link['source'] = nodeById.get(ware);
+        link['target'] = nodeById.get(other_ware);
+        link['probability'] = probability;
+        link['stroke'] = probability_to_color(probability);
+        link['strokewidth'] = probability_to_strokewidth(probability);
+        graph['links'].push(link);
     });
 
 
@@ -99,60 +99,60 @@ function build_page(bonds, densities) {
         .attr('height', HEIGHT);
 
     force
-        .nodes(graph["nodes"])
-        .links(graph["links"])
+        .nodes(graph['nodes'])
+        .links(graph['links'])
         .start();
 
 
-    var link = panel.selectAll(".link")
+    var link = panel.selectAll('.link')
         .data(graph.links)
-      .enter().append("line")
-        .attr("class", function(d){var out = ["link", d.target.id , d.source.id];
-                                   return out.join(" ");})
-        .style("stroke", function(d){return d.stroke})
-        .style("stroke-width", function(d){return d.strokewidth})
-        .style("display" , "none");
+      .enter().append('line')
+        .attr('class', function(d){var out = ['link', d.target.id , d.source.id];
+                                   return out.join(' ');})
+        .style('stroke', function(d){return d.stroke})
+        .style('stroke-width', function(d){return d.strokewidth})
+        .style('display' , 'none');
 
-    var node = panel.selectAll(".node")
+    var node = panel.selectAll('.node')
         .data(graph.nodes)
-        .enter().append("circle")
-        .attr("class", "node")
-        .attr("r", function(d) {return 10 + d.density})
-        .attr("cx", WIDTH/2 )
-        .attr("cy", HEIGHT/2)
-        .style("fill", function(d) {return hash_color(d.density.toString()); })
-        .style("stroke", "black")
-        .style("stroke-width", "1px")
-        .on("click" , function(d){show_ware_info(d['ware_name']);})
-        .on("mouseover", function(d){
-            $("."+d.id.toString()).css("display", "inline");
+        .enter().append('circle')
+        .attr('class', 'node')
+        .attr('r', function(d) {return 10 + d.density})
+        .attr('cx', WIDTH/2 )
+        .attr('cy', HEIGHT/2)
+        .style('fill', function(d) {return hash_color(d.density.toString()); })
+        .style('stroke', 'black')
+        .style('stroke-width', '1px')
+        .on('click' , function(d){show_ware_info(d['ware_name']);})
+        .on('mouseover', function(d){
+            $('.'+d.id.toString()).css('display', 'inline');
             })
-        .on("mouseout", function(){$(".link").css("display", "none")})
+        .on('mouseout', function(){$('.link').css('display', 'none')})
 
         .call(force.drag);
 
 
-    var texts = panel.selectAll("text.label")
+    var texts = panel.selectAll('text.label')
                 .data(graph.nodes)
-                .enter().append("text")
-                .attr("class", "label")
-                .attr("fill", "black")
+                .enter().append('text')
+                .attr('class', 'label')
+                .attr('fill', 'black')
                 .text(function(d) {  return nodeById.get(d.id);  });
 
 
 
 
-      force.on("tick", function() {
-      link.attr("x1", function(d) { return d.source.x; })
-          .attr("y1", function(d) { return d.source.y; })
-          .attr("x2", function(d) { return d.target.x; })
-          .attr("y2", function(d) { return d.target.y; });
+      force.on('tick', function() {
+      link.attr('x1', function(d) { return d.source.x; })
+          .attr('y1', function(d) { return d.source.y; })
+          .attr('x2', function(d) { return d.target.x; })
+          .attr('y2', function(d) { return d.target.y; });
 
-      node.attr("cx", function(d) { return d.x; })
-          .attr("cy", function(d) { return d.y; });
+      node.attr('cx', function(d) { return d.x; })
+          .attr('cy', function(d) { return d.y; });
 
-      texts.attr("transform", function(d) {
-        return "translate(" + d.x + "," + d.y + ")";
+      texts.attr('transform', function(d) {
+        return 'translate(' + d.x + ',' + d.y + ')';
       });
 
       });
@@ -173,14 +173,6 @@ function hash_color(s) {
 
 function show_ware_info(ware_name) {
     infobox.style('visibility', 'visible');
-}
-
-String.prototype.capitalize = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
-}
-
-String.prototype.simplify = function() {
-    return this.replace(/[^A-Z0-9]/ig, '');
 }
 
 function json_to_object(link, callback) {
