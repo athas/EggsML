7d4387a6b0280a154a43bf56f766054a5af5907e
Svip <svip@diku.dk>
2016-04-29 22:17:29 +0200

    mere gofmt

--- concieggs/compiled/vejr.go
+++ concieggs/compiled/vejr.go
@@ -3,156 +3,149 @@
 package main
 
 import (
-    "bytes"
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "net/url"
-    "os"
-    "strings"
-    "text/template"
-    "vejrLib"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"text/template"
+	"vejrLib"
 )
-
 
 const (
-    APIKEY     = "4b42ecc61fd13e0a0cb7006d583fb3e0"
-    kantinevejrBeskrivelse = "Kantinen er inden døre, og der er derfor nok tørvejr og stuetemperatur. Men det ved du nok bedre end jeg!"
+	APIKEY                 = "4b42ecc61fd13e0a0cb7006d583fb3e0"
+	kantinevejrBeskrivelse = "Kantinen er inden døre, og der er derfor nok tørvejr og stuetemperatur. Men det ved du nok bedre end jeg!"
 )
 
-
 type JsonAPI struct {
-    Coord struct {
-        Lon float64
-        Lat float64
-    }
-    Sys struct {
-        Message float64
-        Country string
-        Sunrise int
-        Sunset  int
-    }
-    Weather []struct {
-        Id          int
-        Main        string
-        Description string
-        Icon        string
-    }
-    Base string
-    Main struct {
-        Temp       float64
-        Temp_min   float64
-        Temp_max   float64
-        Pressure   float64
-        Sea_level  float64
-        Grnd_level float64
-        Humidity   int
-    }
-    Wind struct {
-        Speed float64
-        Deg   float64
-    }
-    Clouds struct {
-        All int
-    }
-    Dt   int
-    Id   int
-    Name string
-    Cod  int
+	Coord struct {
+		Lon float64
+		Lat float64
+	}
+	Sys struct {
+		Message float64
+		Country string
+		Sunrise int
+		Sunset  int
+	}
+	Weather []struct {
+		Id          int
+		Main        string
+		Description string
+		Icon        string
+	}
+	Base string
+	Main struct {
+		Temp       float64
+		Temp_min   float64
+		Temp_max   float64
+		Pressure   float64
+		Sea_level  float64
+		Grnd_level float64
+		Humidity   int
+	}
+	Wind struct {
+		Speed float64
+		Deg   float64
+	}
+	Clouds struct {
+		All int
+	}
+	Dt   int
+	Id   int
+	Name string
+	Cod  int
 }
 
-
-
-
 func main() {
-    city := "København"
-    country := "Danmark"
-    if len(os.Args) > 1 {
-        /* Hvad er vejret i Kantinen? */
-        if (os.Args[1] == "Kantinen") {
-            fmt.Println(kantinevejrBeskrivelse)
-            return
-        }
+	city := "København"
+	country := "Danmark"
+	if len(os.Args) > 1 {
+		/* Hvad er vejret i Kantinen? */
+		if os.Args[1] == "Kantinen" {
+			fmt.Println(kantinevejrBeskrivelse)
+			return
+		}
 
-        args := append(os.Args[:0], os.Args[1:]...)
-        argsStr := strings.Join(args, " ")
-        ss := strings.Split(argsStr, ",")
-        if len(ss) == 1 {
-            city = ss[0]
-        }
-        if len(ss) >= 2 {
-            city = strings.Trim(ss[0], " ")
-            country = strings.Trim(ss[1], " ")
-        }
-    }
+		args := append(os.Args[:0], os.Args[1:]...)
+		argsStr := strings.Join(args, " ")
+		ss := strings.Split(argsStr, ",")
+		if len(ss) == 1 {
+			city = ss[0]
+		}
+		if len(ss) >= 2 {
+			city = strings.Trim(ss[0], " ")
+			country = strings.Trim(ss[1], " ")
+		}
+	}
 
-    resp, err := http.Get(fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=%s,%s&lang=da&units=metric&APPID=%s", url.QueryEscape(city), url.QueryEscape(country), APIKEY))
-    if err != nil {
-        fmt.Println("Den by findes ikke eller også er der noget andet, der er gået galt!")
-        return
-    }
-    defer resp.Body.Close()
-    body, _ := ioutil.ReadAll(resp.Body)
+	resp, err := http.Get(fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=%s,%s&lang=da&units=metric&APPID=%s", url.QueryEscape(city), url.QueryEscape(country), APIKEY))
+	if err != nil {
+		fmt.Println("Den by findes ikke eller også er der noget andet, der er gået galt!")
+		return
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
 
-    var dat JsonAPI
-    err = json.Unmarshal(body, &dat)
-    if err != nil {
-        fmt.Println("Den by findes ikke eller også er der noget andet, der er gået galt!")
-        return
-    }
+	var dat JsonAPI
+	err = json.Unmarshal(body, &dat)
+	if err != nil {
+		fmt.Println("Den by findes ikke eller også er der noget andet, der er gået galt!")
+		return
+	}
 
+	/* Hent relevant vinddata fra JSON-struktur */
+	degrees := dat.Main.Temp
+	windSpeed := dat.Wind.Speed
+	windBeaufortName := vejrLib.Beaufort(dat.Wind.Speed)
+	windDirection := dat.Wind.Deg
 
-    /* Hent relevant vinddata fra JSON-struktur */
-    degrees := dat.Main.Temp
-    windSpeed := dat.Wind.Speed
-    windBeaufortName := vejrLib.Beaufort(dat.Wind.Speed)
-    windDirection := dat.Wind.Deg
+	/* Hent vejrbeskrivelse, fortolk og fordansk denne */
+	description := dat.Weather[0].Id
+	beskrivelse := vejrLib.Vejrbeskrivelse(description)
 
-    /* Hent vejrbeskrivelse, fortolk og fordansk denne */
-    description := dat.Weather[0].Id
-    beskrivelse := vejrLib.Vejrbeskrivelse(description)
+	/* Hent coordinater for målestation og udregn afstand til Kantinen */
+	lon := dat.Coord.Lon
+	lat := dat.Coord.Lat
+	afstandStr := vejrLib.AfstandStr(lon, lat)
 
-    /* Hent coordinater for målestation og udregn afstand til Kantinen */
-    lon := dat.Coord.Lon
-    lat := dat.Coord.Lat
-    afstandStr := vejrLib.AfstandStr(lon, lat)
+	/* Tid for opdatering */
+	timeForUpdate := dat.Dt
+	ageStr := vejrLib.ErMaalingenGammel(timeForUpdate)
 
-    /* Tid for opdatering */
-    timeForUpdate := dat.Dt
-    ageStr := vejrLib.ErMaalingenGammel(timeForUpdate)
+	windDirectionstr := vejrLib.WindDirectionString(windDirection)
 
+	t, _ := template.New("vejr").Parse(`Vejret i {{.City}}, {{.Country}}: {{.Beskrivelse}} med en temperatur på {{.Degrees}}°C. {{.WindBeaufortName}}, {{.WindSpeed}} m/s, fra {{.WindDirection}}. {{.Afstand}} {{.Age}}`)
+	// t, _ := template.New("vejr").Parse(`Vejret i {{.City}}, {{.Country}}: {{.Beskrivelse}} med en temperatur på {{.Degrees}}°C. {{.WindBeaufortName}}, {{.WindSpeed}} m/s, fra {{.WindDirection}}. {{.Afstand}} ({{.Position}}) {{.Age}}`)
 
-    windDirectionstr := vejrLib.WindDirectionString(windDirection)
+	out := bytes.NewBufferString("")
+	t.Execute(out, struct {
+		City             string
+		Country          string
+		Beskrivelse      string
+		Degrees          string
+		WindBeaufortName string
+		WindSpeed        string
+		WindDirection    string
+		Afstand          string
+		//Position          string
+		Age string
+	}{
+		city,
+		country,
+		beskrivelse,
+		fmt.Sprintf("%.1f", degrees),
+		fmt.Sprint(windBeaufortName),
+		fmt.Sprintf("%.1f", windSpeed),
+		windDirectionstr,
+		afstandStr,
+		//fmt.Sprintf("længdegrad: %.3f°, breddegrad: %.3f°.", lon, lat),
+		ageStr,
+	})
 
-    t, _ := template.New("vejr").Parse(`Vejret i {{.City}}, {{.Country}}: {{.Beskrivelse}} med en temperatur på {{.Degrees}}°C. {{.WindBeaufortName}}, {{.WindSpeed}} m/s, fra {{.WindDirection}}. {{.Afstand}} {{.Age}}`)
-        // t, _ := template.New("vejr").Parse(`Vejret i {{.City}}, {{.Country}}: {{.Beskrivelse}} med en temperatur på {{.Degrees}}°C. {{.WindBeaufortName}}, {{.WindSpeed}} m/s, fra {{.WindDirection}}. {{.Afstand}} ({{.Position}}) {{.Age}}`)
-
-    out := bytes.NewBufferString("")
-    t.Execute(out, struct {
-        City              string
-        Country           string
-        Beskrivelse       string
-        Degrees           string
-        WindBeaufortName  string
-        WindSpeed         string
-        WindDirection     string
-        Afstand           string
-        //Position          string
-        Age               string
-    }{
-        city,
-        country,
-        beskrivelse,
-        fmt.Sprintf("%.1f", degrees),
-        fmt.Sprint(windBeaufortName),
-        fmt.Sprintf("%.1f", windSpeed),
-        windDirectionstr,
-        afstandStr,
-        //fmt.Sprintf("længdegrad: %.3f°, breddegrad: %.3f°.", lon, lat),
-        ageStr,
-    })
-
-    fmt.Println(out.String())
+	fmt.Println(out.String())
 }
 
--- concieggs/compiled/vejrudsigt.go
+++ concieggs/compiled/vejrudsigt.go
@@ -1,23 +1,23 @@
 package main
 
-import(
-    "fmt"
-    "net/http"
-    "io/ioutil"
-    "net/url"
-    "encoding/json"
-    //"math"
-    "os"
-    "strings"
-    //"text/template"
-    //"bytes"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	//"math"
+	"os"
+	"strings"
+	//"text/template"
+	//"bytes"
 )
 
 const (
-    APIKEY     = "4b42ecc61fd13e0a0cb7006d583fb3e0"
-    OPLOESNING = 22.5
-    fejlVedVejrBeskrivelse    = "Jeg kender ikke denne vejrbeskrivelse."
-    APIURL = "http://api.openweathermap.org/data/2.5/forecast?q=%s,%s&lang=da&mode=json&units=metric&APPID=%s"
+	APIKEY                 = "4b42ecc61fd13e0a0cb7006d583fb3e0"
+	OPLOESNING             = 22.5
+	fejlVedVejrBeskrivelse = "Jeg kender ikke denne vejrbeskrivelse."
+	APIURL                 = "http://api.openweathermap.org/data/2.5/forecast?q=%s,%s&lang=da&mode=json&units=metric&APPID=%s"
 )
 
 /* Følgende kommentar viser strukturen af den hentede data */
@@ -62,120 +62,116 @@ type JsonAPI struct {
 */
 
 func getAdvance(arg2 string) {
-    advance := -1
-    if (tidspunkt == "i dag") {
-        advance = 0
-    }
-    if (tidspunkt == "i morgen") {
-        advance = 1
-    }
-    if (tidspunkt == "i overmorgen") {
-        advance = 2
-    }
-    return advance
+	advance := -1
+	if tidspunkt == "i dag" {
+		advance = 0
+	}
+	if tidspunkt == "i morgen" {
+		advance = 1
+	}
+	if tidspunkt == "i overmorgen" {
+		advance = 2
+	}
+	return advance
 }
-
 
 func main() {
-    city := "København"
-    country := "Danmark"
+	city := "København"
+	country := "Danmark"
 
-    if (len(os.Args) < 3) {
-        fmt.Println("Brug: vejrudsigt <i dag|i morgen|i overmorgen> [, by[, land]]")
-        return
-    }
+	if len(os.Args) < 3 {
+		fmt.Println("Brug: vejrudsigt <i dag|i morgen|i overmorgen> [, by[, land]]")
+		return
+	}
 
-    args := append(os.Args[:0], os.Args[1:]...) //what does this do??
-    argsStr := strings.Join(args, " ")
-    ss := strings.Split(argsStr, ",")
-    tidspunkt := ""
-    if len(ss) == 1 {
-        tidspunkt = ss[0]
-    }
-    if len(ss) == 2 {
-        tidspunkt = strings.Trim(ss[0], " ")
-        city = strings.Trim(ss[1], " ")
-    }
-    if len(ss) == 3 {
-        tidspunkt = strings.Trim(ss[0], " ")
-        city = strings.Trim(ss[1], " ")
-        country = strings.Trim(ss[2], " ")
-    }
+	args := append(os.Args[:0], os.Args[1:]...) //what does this do??
+	argsStr := strings.Join(args, " ")
+	ss := strings.Split(argsStr, ",")
+	tidspunkt := ""
+	if len(ss) == 1 {
+		tidspunkt = ss[0]
+	}
+	if len(ss) == 2 {
+		tidspunkt = strings.Trim(ss[0], " ")
+		city = strings.Trim(ss[1], " ")
+	}
+	if len(ss) == 3 {
+		tidspunkt = strings.Trim(ss[0], " ")
+		city = strings.Trim(ss[1], " ")
+		country = strings.Trim(ss[2], " ")
+	}
 
-    advance := getAdvance(ss[0])
-    if (advance == -1) {
-        fmt.Println("Gyldige tidspunkter er \"i morgen\" eller \"i overmorgen\"")
-        return
-    }
+	advance := getAdvance(ss[0])
+	if advance == -1 {
+		fmt.Println("Gyldige tidspunkter er \"i morgen\" eller \"i overmorgen\"")
+		return
+	}
 
-    resp, err := http.Get(fmt.Sprintf(APIURL, url.QueryEscape(city), url.QueryEscape(country), APIKEY))
-    if err != nil {
-        fmt.Println("1 Den by findes ikke eller også er der noget andet, der er gået galt!")
-        return
-    }
+	resp, err := http.Get(fmt.Sprintf(APIURL, url.QueryEscape(city), url.QueryEscape(country), APIKEY))
+	if err != nil {
+		fmt.Println("1 Den by findes ikke eller også er der noget andet, der er gået galt!")
+		return
+	}
 
-    defer resp.Body.Close()
-    body, _ := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
 
-    /* En dynamisk struktur er umiddelbart nødvendig, da API'en ikke altid returnerer den samme struktur (rain feltet indgår ikke altid) */
-    type Datif map[string]interface{}
-    var dat Datif
-    json.Unmarshal(body,&dat)
+	/* En dynamisk struktur er umiddelbart nødvendig, da API'en ikke altid returnerer den samme struktur (rain feltet indgår ikke altid) */
+	type Datif map[string]interface{}
+	var dat Datif
+	json.Unmarshal(body, &dat)
 
-    if err != nil {
-        fmt.Println("2 Den by findes ikke eller også er der noget andet, der er gået galt!")
-        //return
-    }
+	if err != nil {
+		fmt.Println("2 Den by findes ikke eller også er der noget andet, der er gået galt!")
+		//return
+	}
 
-    fmt.Println(dat)
+	fmt.Println(dat)
 
+	/* Hent relevant vinddata fra JSON-struktur */
+	// city = (dat["city"].(map[string]interface{}))["name"].(string)
+	// country =  (dat["city"].(map[string]interface{}))["country"].(string)
+	listn := dat["List"].([]interface{})
+	/* How do you weather data for the current day?? Some date library must be used. */
+	/* The following call should only take weather forecasts for today if "i dag" is 0th argument */
+	list := listn[advance].(map[string]interface{}) //entry no. 0-2 in
 
-    /* Hent relevant vinddata fra JSON-struktur */
-    // city = (dat["city"].(map[string]interface{}))["name"].(string)
-    // country =  (dat["city"].(map[string]interface{}))["country"].(string)
-    /listn := dat["List"].([]interface{})
-    /* How do you weather data for the current day?? Some date library must be used. */
-    /* The following call should only take weather forecasts for today if "i dag" is 0th argument */
-    list := listn[advance].(map[string]interface{}) //entry no. 0-2 in
+	// temp := list["temp"].(map[string]interface{})
+	// degrees := temp["day"].(float64)
 
-    // temp := list["temp"].(map[string]interface{})
-    // degrees := temp["day"].(float64)
+	// weathern := list["weather"].([]interface{})
+	// weather := weathern[0].(map[string]interface{})
+	// kode := weather["id"].(float64) // SHOULD BE INT!! But only works for float!
+	// beskrivelse := vejrbeskrivelse(int(kode))
 
-    // weathern := list["weather"].([]interface{})
-    // weather := weathern[0].(map[string]interface{})
-    // kode := weather["id"].(float64) // SHOULD BE INT!! But only works for float!
-    // beskrivelse := vejrbeskrivelse(int(kode))
+	// windSpeed := list["speed"].(float64)
+	// windBeaufortName := beaufort(windSpeed)
+	// windDirection := list["deg"].(float64)
+	// windDirectionStr := windDirectionString(windDirection)
 
-    // windSpeed := list["speed"].(float64)
-    // windBeaufortName := beaufort(windSpeed)
-    // windDirection := list["deg"].(float64)
-    // windDirectionStr := windDirectionString(windDirection)
+	// t, _ := template.New("vejr").Parse(`Vejret i {{.City}}, {{.Country}}, {{.Tidspunkt}}: {{.Beskrivelse}} med en forventet temperatur på {{.Degrees}}°C. {{.WindBeaufortName}}, {{.WindSpeed}} m/s, fra {{.WindDirection}}.`)
+	// out := bytes.NewBufferString("")
+	// t.Execute(out, struct {
+	//     City              string
+	//     Country           string
+	//     Tidspunkt         string
+	//     Beskrivelse       string
+	//     Degrees           int
+	//     WindBeaufortName  string
+	//     WindSpeed         string
+	//     WindDirection     string
+	// }{
+	//     city,
+	//     country,
+	//     tidspunkt, //sat af funktionskald
+	//     beskrivelse,
+	//     int(degrees),
+	//     fmt.Sprint(windBeaufortName),
+	//     fmt.Sprintf("%.1f", windSpeed),
+	//     windDirectionStr,
+	// })
 
-
-    // t, _ := template.New("vejr").Parse(`Vejret i {{.City}}, {{.Country}}, {{.Tidspunkt}}: {{.Beskrivelse}} med en forventet temperatur på {{.Degrees}}°C. {{.WindBeaufortName}}, {{.WindSpeed}} m/s, fra {{.WindDirection}}.`)
-    // out := bytes.NewBufferString("")
-    // t.Execute(out, struct {
-    //     City              string
-    //     Country           string
-    //     Tidspunkt         string
-    //     Beskrivelse       string
-    //     Degrees           int
-    //     WindBeaufortName  string
-    //     WindSpeed         string
-    //     WindDirection     string
-    // }{
-    //     city,
-    //     country,
-    //     tidspunkt, //sat af funktionskald
-    //     beskrivelse,
-    //     int(degrees),
-    //     fmt.Sprint(windBeaufortName),
-    //     fmt.Sprintf("%.1f", windSpeed),
-    //     windDirectionStr,
-    // })
-
-    // fmt.Println(out.String())
-
-
+	// fmt.Println(out.String())
 
 }
+
