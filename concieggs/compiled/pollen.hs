0372a78f54c2006b12a071f1b3c1a60b19911869
Christian Kjær Larsen <christianden@gmail.com>
2016-06-26 14:59:33 +0200

    Allergikernyheder

---
+++ concieggs/compiled/pollen.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Network.HTTP
+import Text.XML.Light
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Text.Encoding as TEN
+
+url = "http://www.dmi.dk/vejr/services/pollen-rss/"
+
+downloadPollen :: IO String
+downloadPollen = Network.HTTP.simpleHTTP (getRequest url) >>= getResponseBody
+
+findPollen :: Maybe Element -> Maybe T.Text
+findPollen e = e >>= findElement (unqual "item")
+                 >>= findElement (unqual "description")
+                 >>= return . (T.intercalate " ")
+                            . (filter (\x -> x /= ""))
+                            . (map T.strip)
+                            . T.lines . TEN.decodeUtf8 . BS.pack . strContent
+
+printResult :: Maybe T.Text -> IO ()
+printResult (Just s) = TIO.putStrLn "Atjuu!" >> TIO.putStrLn s
+printResult Nothing = TIO.putStrLn "Ingen pollental!"
+
+main :: IO ()
+main = downloadPollen >>= (printResult . findPollen . parseXMLDoc)
