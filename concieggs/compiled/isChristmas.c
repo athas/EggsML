0836bf4c1f5d05d4faa6edc87197a3c3cd9fce91
Simon Shine <shreddedglory@gmail.com>
2016-11-30 22:13:57 +0100

    Max ud juleaften. #74
    - Juleaften er det 150% jul! (@svip)
    - Juleaften varer hele 24. december og indtil 25. december kl. 06:00.
    - Omdøb enkelte variable (rawtoday -> raw_today, maxdays -> max_days).
    - Introducér WITHIN_TIME_INTERVAL(begin, end, x)-makro.

--- concieggs/compiled/isChristmas.c
+++ concieggs/compiled/isChristmas.c
@@ -1,3 +1,6 @@
+// Genererer et juleniveau mellem 0 og 150.  Desto tættere på 100, desto mere
+// julet. Virker kun mellem 24. november og 25. december, begge dage inklusiv.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -5,34 +8,49 @@
 #include <math.h>
 #include <string.h>
 
-// Genererer et juleniveau mellem 0 og 100.  Desto tættere på 100, desto mere
-// julet. Virker kun mellem 24. november og 25. december, begge dage inklusiv.
-
 // struct tm's .tm_mon ligger i intervallet [0, 11].
 #define NOVEMBER (10)
 #define DECEMBER (11)
 
-double christmasLevel(time_t rawtoday)
-{
-    struct tm begin, end, *today;
-    time_t rawbegin, rawend;
-    double days, maxdays, level;
+/* x_time is within the time interval [begin_time, end_time]. */
+#define WITHIN_TIME_INTERVAL(begin_time, x_time, end_time) \
+  (difftime((end_time), (x_time)) >= 0 && \
+   difftime((x_time), (begin_time)) >= 0)
 
-    today = localtime(&rawtoday);
+double christmasLevel(time_t raw_today)
+{
+    struct tm *today, begin, end, eve_begin, eve_end;
+    time_t raw_begin, raw_end, raw_eve_begin, raw_eve_end;
+    double days, max_days, level;
+
+    today = localtime(&raw_today);
     begin = (struct tm) { 0, 0, 0, 24, NOVEMBER, today->tm_year, 0, 0, 0 };
     end = (struct tm) { 0, 0, 0, 26, DECEMBER, today->tm_year, 0, 0, 0 };
 
-    rawbegin = mktime(&begin);
-    rawend = mktime(&end);
+    raw_begin = mktime(&begin);
+    raw_end = mktime(&end);
 
-    if (difftime(rawend, rawtoday) < 0 || difftime(rawtoday, rawbegin) < 0) {
+    if (!WITHIN_TIME_INTERVAL(raw_begin, raw_today, raw_end)) {
         return 0;
     }
 
-    days = difftime(rawtoday, rawbegin) / (60*60*24);
-    maxdays = difftime(rawend, rawbegin) / (60*60*24);
+    // Let Christmas eve be [YYYY-12-24 00:00:00 ; YYYY-12-25 06:00:00.
+    eve_begin = (struct tm) { 0, 0, 0, 24, DECEMBER, today->tm_year, 0, 0, 0 };
+    eve_end = (struct tm) { 0, 0, 6, 25, DECEMBER, today->tm_year, 0, 0, 0 };
 
-    level = pow(1.1, days) / pow(1.1, maxdays);
+    raw_eve_begin = mktime(&eve_begin);
+    raw_eve_end = mktime(&eve_end);
+
+    // Max out Christmas level on Christmas eve.
+    if (WITHIN_TIME_INTERVAL(raw_eve_begin, raw_today, raw_eve_end)) {
+        return 150;
+    }
+
+
+    days = difftime(raw_today, raw_begin) / (60*60*24);
+    max_days = difftime(raw_end, raw_begin) / (60*60*24);
+
+    level = pow(1.1, days) / pow(1.1, max_days);
 
     // Boost christmas level on Advent Sundays!
     if (today->tm_wday == 0) {
@@ -47,7 +65,7 @@ double christmasLevel(time_t rawtoday)
         raw_in_four_weeks = mktime(&in_four_weeks);
         raw_end_of_advent = mktime(&end_of_advent);
 
-        if (difftime(raw_end_of_advent, rawtoday) > 0 || difftime(raw_in_four_weeks, raw_end_of_advent) > 0) {
+        if (difftime(raw_end_of_advent, raw_today) > 0 || difftime(raw_in_four_weeks, raw_end_of_advent) > 0) {
             level = level > 0.8 ? level : 0.8;
         }
     }
@@ -55,20 +73,20 @@ double christmasLevel(time_t rawtoday)
     return 100*level;
 }
 
-bool isChristmas(time_t rawtoday)
+bool isChristmas(time_t raw_today)
 {
-    return christmasLevel(rawtoday) > 0;
+    return christmasLevel(raw_today) > 0;
 }
 
 int main(int argc, char *argv[])
 {
-    time_t rawtoday = time(NULL);
+    time_t raw_today = time(NULL);
 
     if (strstr(argv[0], "christmasLevel") != NULL) {
-        printf("%.2f", christmasLevel(rawtoday));
+        printf("%.2f", christmasLevel(raw_today));
         return 0;
     } else if (strstr(argv[0], "isChristmas") != NULL) {
-        return !isChristmas(rawtoday); // 0 is success
+        return !isChristmas(raw_today); // 0 is success
     }
 
     return 0;
