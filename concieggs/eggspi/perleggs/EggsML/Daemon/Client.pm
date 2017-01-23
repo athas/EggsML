b4789b129241007a22ef8df5bf5b2e4db1a98378
Sebastian Paaske Tørholm <Eckankar@gmail.com>
2017-01-23 22:18:53 +0100

    Docs!

--- concieggs/eggspi/perleggs/EggsML/Daemon/Client.pm
+++ concieggs/eggspi/perleggs/EggsML/Daemon/Client.pm
@@ -6,6 +6,14 @@ use Env qw/EGGS_DAEMON_SOCKET/;
 use IO::Socket::UNIX;
 use utf8::all;
 
+=head1 NAME
+
+EggsML::Daemon::Client - Access the EggsML daemon directly from perl.
+
+=head1 METHODS
+
+=cut
+
 sub _run {
     my ($self, @command) = @_;
 
@@ -45,12 +53,25 @@ sub _run_bool {
     return $resCode ? 0 : 1;
 }
 
+=head2 aliases
+
+Get a list of the aliases of te people given.
+
+=cut
+
+
 sub aliases {
     my ($self, @names) = @_;
     my ($resCode, $res) = $self->_run_nested_list( 'aliases', @names );
     return if $resCode;
     return $res;
 }
+
+=head2 wishes
+
+Get a list of all the wishes.
+
+=cut
 
 sub wishes {
     my $self = shift;
@@ -59,12 +80,22 @@ sub wishes {
     return $res;
 }
 
+=head2 balances
+
+Get a hash of all the people in Eggs' balances.
+
+=cut
+
 sub balances {
     my $self = shift;
     my ($resCode, $res) = $self->_run_hash( 'balances' );
     return if $resCode;
     return { reverse %$res };
 }
+
+=head2 balance_of_payments
+
+=cut
 
 sub balance_of_payments {
     my $self = shift;
@@ -73,12 +104,21 @@ sub balance_of_payments {
     return $res;
 }
 
+=head2 allaliases
+
+Get a list of lists; each containing all aliases for one member.
+=cut
+
 sub allaliases {
     my $self = shift;
     my ($resCode, $res) = $self->_run_nested_list( 'allaliases' );
     return if $resCode;
     return $res;
 }
+
+=head2 lunches
+
+=cut
 
 sub lunches {
     my ($self, $name) = @_;
@@ -87,12 +127,22 @@ sub lunches {
     return $res;
 }
 
+=head2 eggsmates
+
+=cut
+
 sub eggsmates {
     my ($self, $name) = @_;
     my ($resCode, $res) = $self->_run( 'eggsmates', $name );
     return if $resCode;
     return $res;
 }
+
+=head2 eggscount
+
+Get the eggscount for a given person.
+
+=cut
 
 sub eggscount {
     my ($self, $name) = @_;
@@ -101,6 +151,10 @@ sub eggscount {
     return $res;
 }
 
+=head2 consecutive
+
+=cut
+
 sub consecutive {
     my ($self, $name) = @_;
     my ($resCode, $res) = $self->_run( 'consecutive', $name );
@@ -108,10 +162,21 @@ sub consecutive {
     return $res;
 }
 
+=head2 cmpnames
+
+Is person1 equal to person2?
+=cut
+
 sub cmpnames {
     my ($self, $name1, $name2) = @_;
     return $self->_run_bool( 'cmpnames', $name1, $name2 );
 }
+
+=head2 isTrusted
+
+Is the given person trusted?
+
+=cut
 
 sub isTrusted {
     my ($self, $name) = @_;
