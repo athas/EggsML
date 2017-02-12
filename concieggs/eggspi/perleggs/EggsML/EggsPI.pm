dd5e8a31a3e44859c698f2b82307ce402cdddb1d
Sebastian Paaske Tørholm <Eckankar@gmail.com>
2017-02-12 23:31:36 +0100

    Initial draft for EggsML::EggsPI
    Issue #62
    
    Usage:
    
    ```
        #!/usr/bin/env perl
        use 5.022;
        use warnings;
        use YAML;
    
        use EggsML::EggsPI
            boolean => [ qw/isChristmas/ ],
                list    => [ qw/annoyances/ ],
                    text    => [ qw/lookupDebt/ ],
                        raw     => [ qw/randomName/ ];
    
        # Treat return code as a boolean (0 = true; anything else = false)
        say isChristmas() ? "yep" : "nay";
    
        # Treat resulting output as a list (each line = entry in list)
        say YAML::Dump( annoyances("Eckankar") );
    
        # Treat as a string output and trim off newlines
        say YAML::Dump( lookupDebt("Eckankar") );
    
        # Give raw output from command
        say YAML::Dump( randomName("Eckankar") );
    ```

---
+++ concieggs/eggspi/perleggs/EggsML/EggsPI.pm
@@ -0,0 +1,54 @@
+#!/usr/bin/env perl
+package EggsML::EggsPI;
+use 5.020;
+use warnings;
+
+use Exporter;
+use IPC::System::Simple qw/system capture EXIT_ANY/;
+use Sub::Install;
+
+sub _install_sub_handler {
+    my ($type, $caller, $code) = @_;
+
+    for my $method (@{ $type // [] }) {
+        Sub::Install::install_sub({
+            code => sub { $code->($method, @_) },
+            into => $caller,
+            as   => $method,
+        });
+    }
+}
+
+sub import {
+    my ($package, %args) = @_;
+    my ($caller) = caller;
+
+    _install_sub_handler($args{boolean}, $caller, sub {
+        my $method = shift;
+        my $exit_code = system(EXIT_ANY, $method, @_);
+        return ! $exit_code;
+    } );
+
+    _install_sub_handler($args{list}, $caller, sub {
+        my $method = shift;
+        chomp(my @lines = capture(EXIT_ANY, $method, @_));
+        return @lines;
+    } );
+
+    _install_sub_handler($args{text}, $caller, sub {
+        my $method = shift;
+        chomp(my $text = capture(EXIT_ANY, $method, @_));
+        return $text;
+    } );
+
+    _install_sub_handler($args{raw}, $caller, sub {
+        my $method = shift;
+        my $text = capture(EXIT_ANY, $method, @_);
+        return $text;
+    } );
+}
+
+
+
+1;
+
