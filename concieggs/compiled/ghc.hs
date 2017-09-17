e9a889e9c61b973782a43dfdd165ccc995182d40
Troels Henriksen <athas@sigkill.dk>
2017-09-17 20:03:59 +0200

    pimp

--- concieggs/compiled/ghc.hs
+++ concieggs/compiled/ghc.hs
@@ -6,6 +6,8 @@ import Data.List
 import Control.Monad
 import System.Environment
 
+data FunctionIsh a = Atom a | Arrow (FunctionIsh a) (FunctionIsh a)
+
 goodModules :: [String]
 goodModules = [ "funkyGen"
               , "Curves"
@@ -39,47 +41,70 @@ dataTypes :: [String]
 dataTypes = []
 
 
-function :: Int -> IO String
+function :: Int -> Gen String
 function n = do
-  n <- generate $ choose (1,n)
-  typeclass <- generate $ choose (True, False)
-  types <- generate $ someTypes n
+  n <- choose (1,n)
+  typeclass <- choose (True, False)
+  types <- someTypes n
   let types' = concat $ intersperse (" " :: String) $ intersperse ("->" :: String) types
   return types'
 
-twoTyped :: Int -> IO String
+twoTyped :: Int -> Gen String
 twoTyped n = do
-  twotyped <- generate $ elements twoTypedData
-  [a, b] <- generate $ someTypes 2
+  twotyped <- elements twoTypedData
+  [a, b] <- someTypes 2
   return $ printf "%s %s %s" twotyped a b
 
-funkyType :: Int -> IO String
-funkyType 0 = generate someType
+funkyType :: Int -> Gen String
+funkyType 0 = someType
 funkyType n = do
-  nesting <- generate $ choose (0,n)
+  nesting <- choose (0,n)
   twotpd <- twoTyped nesting
   lst <- list nesting
-  generate $ frequency [(1, pure twotpd), (2, pure lst)]
+  frequency [(1, pure twotpd), (2, pure lst)]
 
-list :: Int -> IO String
-list 0 = do tp <- generate someType; return $ printf "[%s]" tp
+funkyKind :: Gen String
+funkyKind = do
+  frequency [(1, return "*"), (2, ("* -> "++) <$> funkyKind)]
+
+list :: Int -> Gen String
+list 0 = do tp <- someType; return $ printf "[%s]" tp
 list n = do lst <- list (n-1); return $ printf "[%s]" lst
 
-
-funnyFunk :: String -> IO ()
-funnyFunk filename = do
-  [linje, start] <- generate $ vectorOf 2 $ choose (1, 100) :: IO [Int]
-  slut <- generate $ choose (start, 100)
+simpleError :: String -> Gen String
+simpleError filename = do
+  [linje, start] <- vectorOf 2 $ choose (1, 100) :: Gen [Int]
+  slut <- choose (start, 100)
   expected <- function 2
   actual <- function 4
   fun_type <- funkyType 1
   more_fun_type <- funkyType 2
 
-  putStrLn $ printf "%s:%d:%d-%d: error ..." filename linje start slut
-  putStrLn $ printf "    * Couldn't match type '%s' with '%s'" expected actual
-  putStrLn $ printf "      Expected type: %s" fun_type
-  putStrLn $ printf "        Actual type: %s" more_fun_type
-  putStrLn "..."
+  return $ unlines [ printf "%s:%d:%d-%d: error ..." filename linje start slut
+                   , printf "    * Couldn't match type '%s' with '%s'" expected actual
+                   , printf "      Expected type: %s" fun_type
+                   , printf "        Actual type: %s" more_fun_type
+                   , "..." ]
+
+kindSignatureError :: String -> Gen String
+kindSignatureError filename = do
+  kind <- funkyKind
+  tfam <- someType
+  [linje, start] <- vectorOf 2 $ choose (1, 100) :: Gen [Int]
+  return $ unlines [ printf "%s:%d:%d: error:" filename linje start
+                   , printf "Illegal kind signature: ‘%s’" kind
+                   , "Perhaps you intended to use KindSignatures"
+                   , printf "In the declaration for type family ‘%s’" tfam
+                   ]
+
+errorMessage :: String -> Gen String
+errorMessage filename =
+  frequency [(5, simpleError filename),
+             (1, kindSignatureError filename)]
+
+funnyFunk :: String -> IO ()
+funnyFunk filename = do
+  putStr =<< generate (errorMessage filename)
 
 main :: IO ()
 main = do
