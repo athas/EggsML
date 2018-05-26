384d884e244fded428f97677f490e3561cb05edc
Svip <svip@diku.dk>
2018-05-26 23:28:53 +0200

    Nu med alle kendte sager om ebola!

--- concieggs/compiled/ebola.go
+++ concieggs/compiled/ebola.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	ebolaUrl = "http://en.wikipedia.org/w/index.php?title=West_African_Ebola_virus_epidemic&action=raw"
+	ebolaUrl = "https://en.wikipedia.org/wiki/List_of_Ebola_outbreaks"
 )
 
 func main() {
@@ -27,7 +27,7 @@ func main() {
 		fmt.Println("Øv.  Jeg fik noget lort tilbage.")
 		return
 	}
-	reg, _ := regexp.Compile("Total \\| ([0-9,]+) \\| ([0-9,]+)")
+	reg, _ := regexp.Compile("(?s)<tr>.*?<td>.*?</td>.*?<td>.*?</td>.*?<td>.*?</td>.*?<td>.*?([0-9,]+).*?</td>.*?<td>.*?([0-9,]+).*?</td>.*?<td>.*?</td>.*?<td>.*?</td>.*?</tr>")
 	if reg == nil {
 		fmt.Println("Nå.  Der er ingen tal.")
 		return
@@ -35,22 +35,41 @@ func main() {
 	res := reg.FindAllSubmatch(body, -1)
 	if len(res) < 1 {
 		fmt.Println("Nå.  Der var ingen tal.")
-	}
-	cases, err := strconv.Atoi(bytes.NewBuffer(bytes.Replace(res[0][1], []byte(","), []byte(""), -1)).String())
-	if err != nil {
-		fmt.Println("Katastrofe!  Tallene er i udu.")
 		return
 	}
-	deaths, err := strconv.Atoi(bytes.NewBuffer(bytes.Replace(res[0][2], []byte(","), []byte(""), -1)).String())
-	if err != nil {
-		fmt.Println("Katastrofe!  Tallene er i udu.")
-		return
+	var cases, deaths int
+	for _, e := range res {
+		tcases, err := strconv.Atoi(bytes.NewBuffer(bytes.Replace(e[1], []byte(","), []byte(""), -1)).String())
+		if err != nil {
+			fmt.Println("Katastrofe!  Tallene er i udu.")
+			return
+		}
+		tdeaths, err := strconv.Atoi(bytes.NewBuffer(bytes.Replace(e[2], []byte(","), []byte(""), -1)).String())
+		if err != nil {
+			fmt.Println("Katastrofe!  Tallene er i udu.")
+			return
+		}
+		cases += tcases
+		deaths += tdeaths
 	}
-	fmt.Printf("Ebola har indtil nu smittet %d og slået %d ihjel siden december 2013.\n", cases, deaths)
+	fmt.Printf("Ebola har indtil nu smittet %d og slået %d ihjel.\n", cases, deaths)
 	countries := []string{
+		"Sudan",
+		"Zaire",
+		"Gabon",
+		"Uganda",
+		"Congo",
+		"Det Andet Congo",
 		"Guinea",
 		"Liberia",
 		"Sierra Leone",
+		"Nigeria",
+		"Mali",
+		"Amerikas Forenede Stater",
+		"Senegal",
+		"Spanien",
+		"Storbritanniens og Nordirlands Forenede Kongerige",
+		"Italien",
 	}
 	rand.Seed(time.Now().Unix())
 	fmt.Printf("Du kan hjælpe ved at tage til %s og røre ved nogle mennesker.\n", countries[rand.Intn(len(countries))])
