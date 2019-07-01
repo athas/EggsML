72e44251cdb6e7a6574579fb2ed5d90b63bfc303
Philip Munksgaard <pmunksgaard@gmail.com>
2019-07-01 16:54:06 +0200

    Weights on prefixes

--- concieggs/compiled/programmeringssprog.rust/src/main.rs
+++ concieggs/compiled/programmeringssprog.rust/src/main.rs
@@ -1,18 +1,19 @@
 use rand::seq::SliceRandom;
 
-static PREFIXES: [&str; 12] = [
-    "Visual ",
-    "Microsoft ",
-    "Cloud ",
-    "Objective ",
-    "Turbo ",
-    "Object ",
-    "Standard ",
-    "Common ",
-    "Liquid ",
-    "Meta",
-    "Win",
-    "",
+static PREFIXES: [(&str, u32); 13] = [
+    ("Visual ", 10),
+    ("Microsoft ", 10),
+    ("Cloud ", 10),
+    ("Objective ", 10),
+    ("Turbo ", 10),
+    ("Object ", 10),
+    ("Standard ", 1),
+    ("Common ", 5),
+    ("Liquid ", 1),
+    ("Meta", 2),
+    ("Win", 1),
+    ("Holy", 1),
+    ("", 10),
 ];
 
 static ROOTS: [&str; 38] = [
@@ -49,7 +50,12 @@ fn main() {
 
     let mut result = String::new();
 
-    result.push_str(PREFIXES.choose(&mut rng).unwrap_or(&""));
+    result.push_str(
+        PREFIXES
+            .choose_weighted(&mut rng, |item| item.1)
+            .map(|item| item.0)
+            .unwrap_or(&""),
+    );
 
     result.push_str(ROOTS.choose(&mut rng).unwrap_or(&""));
 
