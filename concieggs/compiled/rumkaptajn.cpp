88958c21fa7c4d61707eb047ecddbb679e3275fb
Niels G. W. Serup <ngws@metanohi.name>
2020-04-02 19:25:06 +0200

    Vær rumkaptajn i C++.

---
+++ concieggs/compiled/rumkaptajn.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main() {
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_int_distribution<> digit(0, 9);
+  uniform_real_distribution<> dist(0.0, 1.0);
+
+  char c;
+  bool rum = false;
+
+  if (dist(gen) < 0.1) {
+    cout << "STJERNEDATO "
+         << digit(gen) << digit(gen) << digit(gen) << digit(gen) << digit(gen)
+         << "." << digit(gen) << digit(gen) << ":\n";
+  }
+  while (cin.read(&c, 1)) {
+    if (rum) {
+      cout << "rum";
+      rum = false;
+    }
+    cout << c;
+    if (c == ' ' && dist(gen) < 0.1) {
+      rum = true;
+    }
+  }
+  return EXIT_SUCCESS;
+}
