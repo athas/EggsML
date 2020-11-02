0762804c91dd97c8507a64af5b76d7bc7294c303
Niels G. W. Serup <ngws@metanohi.name>
2020-11-02 23:24:20 +0100

    Nu laver jeg altså en shell.nix til os NixOS-folk.

---
+++ concieggs/shell.nix
@@ -0,0 +1,41 @@
+# Use this file with nix-shell or similar tools; see https://nixos.org/
+with import <nixpkgs> {};
+
+mkShell {
+  buildInputs = [
+    sic
+    gawk
+    (python27.withPackages (ps: with ps; [
+      numpy
+    ]))
+    (python3.withPackages (ps: with ps; [
+      feedparser yfinance lxml
+    ]))
+    (perl532.withPackages (ps: with ps; [
+      DateTime DateTimeFormatISO8601 Encode Env FileFindRule FilePath
+      FileReadBackwards GitRepository IOAll IPCRun IPCSystemSimple JSON
+      LinguaTranslit ListMoreUtils ListUtilsBy LWP MojoDOM58 StringUtil
+      SubInstall TextAspell TextRoman TextUnaccent TimePiece TryTiny URI
+      YAMLTiny
+    ]))
+    bash
+    jq
+    curl
+    cowsay
+    gcc
+    binutils
+    gnumake
+    go
+    gnu-cobol
+    mosml
+    fpc
+    jdk
+    rustc
+    nim
+    fsharp
+    clang
+    (haskellPackages.ghcWithPackages (ps: with ps; [
+
+    ]))
+  ];
+}
