719b344befe7b117f1e68c942c0465b2918cadb7
sword-smith <thorkilk@gmail.com>
2021-03-09 10:29:25 +0000

    bedre

--- concieggs/eggspi/is_time_between.py
+++ concieggs/eggspi/is_time_between.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 #
 # Returner termineringskode 0 hvis nuværende tid er indeholdt i tidsinterval. Alle tider er CES?T.
+# Forventet input: HH:MM:SS, eksempel: 05:23:19
 
 import pytz
 from datetime import datetime
@@ -18,8 +19,8 @@ cph_now = datetime.now(tz)
 time_zone = cph_now.strftime("%z")
 cph_now = cph_now.time()
 timeStart = sys.argv[1]
-timeStart = datetime.strptime(timeStart + time_zone, "%H:%M%z").time()
 timeEnd = sys.argv[2]
-timeEnd = datetime.strptime(timeEnd + time_zone, "%H:%M%z").time()
+timeStart = datetime.strptime(timeStart + time_zone, "%H:%M:%S%z").time()
+timeEnd = datetime.strptime(timeEnd + time_zone, "%H:%M:%S%z").time()
 
 exit(0 if isNowInTimePeriod(timeStart, timeEnd, cph_now) else 1)
