03ecffbf9f22a1b190ac02505f9a8901ddeca2b7
Einar Rasmussen <einar@di.ku.dk>
2022-06-05 21:57:22 +0200

    Virk igen

--- concieggs/calendar/maerkedage.py
+++ concieggs/calendar/maerkedage.py
@@ -3,16 +3,21 @@
 
 # installér med
 # $ /usr/local/bin/python3.8 -m pip install schedule
+# TODO:
+# 1. kald python filer i mappen `calendar/` så de kan opdateres uafhængigt af concieggsd
+# 2. få topic til at virke
+# 3. tilføj helligdage: se cmds/42royal
+# 4. tilføj helligdage fra Helligdagreformen af 1770.
 
-import schedule
 from time import sleep
+import schedule
 import datetime
 
 
 def begivenhed(besked):
     """Send kontrolbesked"""
     PREFIX = "BEGIVENHED "
-    print(f"{PREFIX}{besked}")
+    print(f"{PREFIX}{besked}\n")
 
 
 def angiv_emne(emne):
@@ -40,7 +45,7 @@ def klokken():
 # Find eksempler her: https://schedule.readthedocs.io/en/stable/examples.html
 schedule.every().day.at("06:00").do(sig, noget="Godmorgen!")
 schedule.every().day.at("22:00").do(sig, noget="Så er det godnat!")
-schedule.every(42).to(420).seconds.do(sig, noget="Hik!")
+schedule.every(42).to(2 * 42).seconds.do(sig, noget="Hik!")
 # schedule.every().hour.at(":00").do(angiv_emne, emne="Klokken er ??:??")
 # schedule.every().hour.at(":30").do(angiv_emne, emne=f"Klokken er {klokken()}")
 
