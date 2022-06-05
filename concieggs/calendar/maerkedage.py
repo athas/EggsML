3bb389131287e656ed48f6fa13906a9a22c0ddec
Einar Rasmussen <einar@di.ku.dk>
2022-06-05 22:03:32 +0200

    Hmm8

--- concieggs/calendar/maerkedage.py
+++ concieggs/calendar/maerkedage.py
@@ -44,10 +44,8 @@ def klokken():
 
 # Find eksempler her: https://schedule.readthedocs.io/en/stable/examples.html
 schedule.every().day.at("06:00").do(sig, noget="Godmorgen!")
-schedule.every().day.at("22:00").do(sig, noget="Så er det godnat!")
+schedule.every().day.at("22:05").do(sig, noget="Så er det godnat!")
 schedule.every(42).to(2 * 42).seconds.do(sig, noget="Hik!")
-# schedule.every().hour.at(":00").do(angiv_emne, emne="Klokken er ??:??")
-# schedule.every().hour.at(":30").do(angiv_emne, emne=f"Klokken er {klokken()}")
 
 
 while True:
