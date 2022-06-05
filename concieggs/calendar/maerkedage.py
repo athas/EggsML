9f51a77a1d7ffd8db54799c3a4039f9b7693b250
Einar Rasmussen <einar@di.ku.dk>
2022-06-05 14:54:04 +0200

    Hold sproget rent. Husk bang i hash-bang.

--- concieggs/calendar/maerkedage.py
+++ concieggs/calendar/maerkedage.py
@@ -1,4 +1,4 @@
-# /usr/bin/env python3.8
+#!/usr/bin/env python3.8
 
 # installér med
 # $ /usr/local/bin/python3.8 -m pip install schedule
@@ -8,14 +8,14 @@ from time import sleep
 import datetime
 
 
-def topic(topic):
+def angiv_emne(emne):
     """Sæt dagens emne"""
-    print(f"/topic {topic}")
+    print(f"/topic {emne}")
 
 
-def say(something):
+def sig(noget):
     """Skriv noget om dagen"""
-    print(f"{something}")
+    print(f"{noget}")
 
 
 def dagens_overblik():
@@ -30,14 +30,14 @@ def klokken():
 
 
 # Find eksempler her: https://schedule.readthedocs.io/en/stable/examples.html
-schedule.every().day.at("06:00").do(say, something="Godmorgen!")
-schedule.every(10).to(60).seconds.do(say, something="Hik!")
-schedule.every().hour.at(":00").do(topic, topic="Klokken er ??:??")
-schedule.every().hour.at(":30").do(topic, topic="Klokken er ??:??")
+schedule.every().day.at("06:00").do(sig, noget="Godmorgen!")
+schedule.every(10).to(60).seconds.do(sig, noget="Hik!")
+schedule.every().hour.at(":00").do(angiv_emne, emne="Klokken er ??:??")
+schedule.every().hour.at(":30").do(angiv_emne, emne=f"Klokken er {klokken()}")
 
 
 while True:
     schedule.run_pending()
-    sleep(60 * 1)
+    sleep(10)
 
 # schedule.run_all(delay_seconds=10)
