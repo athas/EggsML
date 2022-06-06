975bfb903ba49eaad7789a8e6ac6fc87320758f4
Niels G. W. Serup <ngws@metanohi.name>
2022-06-06 15:35:47 +0200

    Afhæng ikke længere af en Haskell-pakke der ikke har været opdateret i ti år.

--- concieggs/eggspi/haskeggs/Concieggs/Random.hs
+++ concieggs/eggspi/haskeggs/Concieggs/Random.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleContexts #-}
 module Concieggs.Random (
@@ -11,30 +12,20 @@ module Concieggs.Random (
   CMR.mkStdGen,
   Random,
   RandomT,
-  randomSt,
   randomR,
   choice,
-  choiceMay,
   choiceM,
-  choiceMMay,
   weightedChoice,
   weightedChoiceMay,
-  sample,
-  shuffle
   ) where
 import Control.Monad
 import qualified Control.Monad.Random as CMR
 import qualified Data.Random as DR
-import qualified Data.Random.Extras as DRE
 import Data.Random.Distribution.Uniform
 import Data.Word (Word64)
 
 type Random a = CMR.Rand CMR.StdGen a
 type RandomT m a = CMR.RandT CMR.StdGen m a
-
--- | Makes a function that returns a DR.RVar usable by e.g. CMR.Rand.
-randomSt :: forall m a . CMR.MonadRandom m => DR.RVar a -> m a
-randomSt rvar = DR.runRVar rvar (CMR.getRandom :: m Word64)
 
 -- | Random number in range @a..b@.
 randomR :: (CMR.Random a, CMR.MonadRandom m) => a -> a -> m a
@@ -42,17 +33,10 @@ randomR a b = CMR.getRandomR (a, b)
 
 -- | Random element from list.
 choice :: CMR.MonadRandom m => [a] -> m a
-choice = randomSt . DRE.choice
+choice = weightedChoice . map (,1)
 
 choiceM :: CMR.MonadRandom m => [m a] -> m a
 choiceM = join . choice
-
--- | Nothing if empty list.
-choiceMay :: CMR.MonadRandom m => [a] -> Maybe (m a)
-choiceMay = liftM randomSt . DRE.safeChoice
-
-choiceMMay :: CMR.MonadRandom m => [m a] -> Maybe (m a)
-choiceMMay = fmap join . choiceMay
 
 -- | Random element from weighted list.
 weightedChoice :: CMR.MonadRandom m => [(a, Rational)] -> m a
@@ -61,14 +45,6 @@ weightedChoice = CMR.fromList
 weightedChoiceMay :: CMR.MonadRandom m => [(a, Rational)] -> Maybe (m a)
 weightedChoiceMay [] = Nothing
 weightedChoiceMay xs = Just (CMR.fromList xs)
-
--- | Random sample from list.
-sample :: CMR.MonadRandom m => Int -> [a] -> m [a]
-sample n = randomSt . DRE.sample n
-
--- | Shuffles list.
-shuffle :: CMR.MonadRandom m => [a] -> m [a]
-shuffle = randomSt . DRE.shuffle
 
 -- | Runs in IO.
 runRandTIO :: Monad m => RandomT m a -> IO (m (a, CMR.StdGen))
