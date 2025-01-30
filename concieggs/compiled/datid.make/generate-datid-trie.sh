02ad2849bcde30c0f81b204d94aa781be55020b6
Niels G. W. Serup <ngws@metanohi.name>
2025-01-30 17:29:55 +0100

    Tilføj en manuel ekstraliste

--- concieggs/compiled/datid.make/generate-datid-trie.sh
+++ concieggs/compiled/datid.make/generate-datid-trie.sh
@@ -2,4 +2,6 @@
 #
 # Lav en trie der går fra nutidsudsagnsord til tilhørende datidsudsagnsord.
 
-generate-trie < $CONCIEGGS_DB_DIR/ordbog-med-bøjninger/unikke_udsagnsord_nutid_datid > "$(dirname "$0")/kode.c"
+{
+    cat $CONCIEGGS_DB_DIR/ordbog-med-bøjninger/unikke_udsagnsord_nutid_datid*
+} | generate-trie > "$(dirname "$0")/kode.c"
