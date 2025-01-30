6507f54227f431723cedfd0599a6036dfec94446
Niels G. W. Serup <ngws@metanohi.name>
2025-01-30 17:17:00 +0100

    Skriv en kommentar så den husker at genbygge rekursivt

--- concieggs/compiled/datid.make/generate-datid-trie.sh
+++ concieggs/compiled/datid.make/generate-datid-trie.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+#
+# Lav en trie der går fra nutidsudsagnsord til tilhørende datidsudsagnsord.
 
 generate-trie < $CONCIEGGS_DB_DIR/ordbog-med-bøjninger/unikke_udsagnsord_nutid_datid > "$(dirname "$0")/kode.c"
